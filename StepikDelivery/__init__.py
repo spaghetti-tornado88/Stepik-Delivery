5cd83760ee53285ca84cf987365853fedf975565
Arthur <56685887+spaghetti-tornado88@users.noreply.github.com>
2020-02-21 13:29:28 +0300

    models import included

--- StepikDelivery/__init__.py
+++ StepikDelivery/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
-from StepikDelivery.extensions import db, migrate, admin
+from StepikDelivery.extensions import migrate, db, admin
 from config import Config
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+from StepikDelivery import models
 
 db.init_app(app)
 migrate.init_app(app, db)
