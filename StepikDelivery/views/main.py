9c07c87168e9eeca99327a7abb574352977d9672
Arthur <56685887+spaghetti-tornado88@users.noreply.github.com>
2020-02-21 16:26:07 +0300

    main.html in progress

--- StepikDelivery/views/main.py
+++ StepikDelivery/views/main.py
@@ -1,9 +1,14 @@
 from flask import render_template
+from sqlalchemy import func
+import random
+
 
 from StepikDelivery import app
-# from StepikDelivery import db
-# from StepikDelivery.models.user import User
+from StepikDelivery.models.category import Category
 
 @app.route('/')
 def main_page():
-    return render_template('main.html')
+    meals_to_show = []
+    for row in Category.query.join(Category.meals):
+        meals_to_show.append(random.sample(row.meals, 3 if len(row.meals) > 3 else len(row.meal)))
+    return render_template('main.html', meals=random.sample(meals_to_show, 4))
